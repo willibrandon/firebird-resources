098ceeab07c5a224f61fafa5de6f86b4e7a68702
Brandon Williams <willibrandon@gmail.com>
2024-10-09 22:34:56 -0700

    Use the latest tag

--- Aspire.Hosting.Firebird/FirebirdContainerImageTags.cs
+++ Aspire.Hosting.Firebird/FirebirdContainerImageTags.cs
@@ -8,12 +8,6 @@ internal static class FirebirdContainerImageTags
     /// <summary>fdcastel/firebird</summary>
     public const string Image = "fdcastel/firebird";
 
-    /// <summary>5.0.1</summary>
-    public const string Tag = "5.0.1";
-
-    ///// <summary>library/phpmyadmin</summary>
-    //public const string PhpMyAdminImage = "library/phpmyadmin";
-
-    ///// <summary>5.2</summary>
-    //public const string PhpMyAdminTag = "5.2";
+    /// <summary>latest</summary>
+    public const string Tag = "latest";
 }

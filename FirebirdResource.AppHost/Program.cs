471ecbf35ded1599b4f3bdddf5d739e45673cbf7
Brandon Williams <willibrandon@gmail.com>
2024-10-13 22:02:40 -0700

    Fix the api service

--- FirebirdResource.AppHost/Program.cs
+++ FirebirdResource.AppHost/Program.cs
@@ -2,21 +2,17 @@ using Firebird.Aspire.Hosting;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
+var firebird = builder.AddFirebird("firebird")
+    .AddDatabase("employees");
+
 var cache = builder.AddRedis("cache");
 
-var apiService = builder.AddProject<Projects.FirebirdResource_ApiService>("apiservice");
-
-var firebird = builder.AddFirebird("firebird", "employees");
-
-var firebirdDb = firebird.AddDatabase("firebirdDb", "employees");
+var apiService = builder.AddProject<Projects.FirebirdResource_ApiService>("apiservice")
+    .WithReference(firebird);
 
 builder.AddProject<Projects.FirebirdResource_Web>("webfrontend")
     .WithExternalHttpEndpoints()
     .WithReference(cache)
-    .WithReference(apiService)
-    .WithReference(firebirdDb);
-
-//builder.AddProject<Projects.FirebirdResource_ApiService>("apiservice")
-//    .WithReference(firebirdDb);
+    .WithReference(apiService);
 
 builder.Build().Run();

4b705c6c0a62332536b55513424954783c843376
Brandon Williams <willibrandon@gmail.com>
2024-10-13 21:24:38 -0700

    Add Aspire test and web frontend projects

--- FirebirdResource.AppHost/Program.cs
+++ FirebirdResource.AppHost/Program.cs
@@ -2,10 +2,21 @@ using Firebird.Aspire.Hosting;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
+var cache = builder.AddRedis("cache");
+
+var apiService = builder.AddProject<Projects.FirebirdResource_ApiService>("apiservice");
+
 var firebird = builder.AddFirebird("firebird", "employees");
+
 var firebirdDb = firebird.AddDatabase("firebirdDb", "employees");
 
-builder.AddProject<Projects.FirebirdResource_ApiService>("apiservice")
+builder.AddProject<Projects.FirebirdResource_Web>("webfrontend")
+    .WithExternalHttpEndpoints()
+    .WithReference(cache)
+    .WithReference(apiService)
     .WithReference(firebirdDb);
+
+//builder.AddProject<Projects.FirebirdResource_ApiService>("apiservice")
+//    .WithReference(firebirdDb);
 
 builder.Build().Run();

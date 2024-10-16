dd3ade33be662d70c3c96a6cb191dad6904f1d61
Brandon Williams <willibrandon@gmail.com>
2024-10-15 21:09:46 -0700

    Simplify the app host

--- FirebirdResource.AppHost/Program.cs
+++ FirebirdResource.AppHost/Program.cs
@@ -3,12 +3,12 @@ using Firebird.Aspire.Hosting;
 var builder = DistributedApplication.CreateBuilder(args);
 
 var firebird = builder.AddFirebird("firebird")
-    .AddDatabase("firebirdDb", "employees");
-
-var cache = builder.AddRedis("cache");
+    .AddDatabase("firebirdDb");
 
 var apiService = builder.AddProject<Projects.FirebirdResource_ApiService>("apiservice")
     .WithReference(firebird);
+
+var cache = builder.AddRedis("cache");
 
 builder.AddProject<Projects.FirebirdResource_Web>("webfrontend")
     .WithExternalHttpEndpoints()

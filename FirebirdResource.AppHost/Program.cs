7f4a0c80ff045cd18143c0489a06541e8b69c745
Brandon Williams <willibrandon@gmail.com>
2024-10-15 21:20:32 -0700

    Create cache, server, database, service, and front end

--- FirebirdResource.AppHost/Program.cs
+++ FirebirdResource.AppHost/Program.cs
@@ -2,13 +2,13 @@ using Firebird.Aspire.Hosting;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
+var cache = builder.AddRedis("cache");
+
 var firebird = builder.AddFirebird("firebird")
     .AddDatabase("firebirdDb");
 
 var apiService = builder.AddProject<Projects.FirebirdResource_ApiService>("apiservice")
     .WithReference(firebird);
-
-var cache = builder.AddRedis("cache");
 
 builder.AddProject<Projects.FirebirdResource_Web>("webfrontend")
     .WithExternalHttpEndpoints()
